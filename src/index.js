bb1bb6ca523b49be94c6d690a1c2e9f8af6fdacf
Tucker Connelly <web@tuckerconnelly.com>
2016-07-02 17:50:18 -0400

    Remove enhanceNetworkLayer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import enhanceNetworkLayer from 'relay-enhance-network-layer'
-
-export default wsrr => enhanceNetworkLayer({
+export default wsrr => ({
   // TODO Handle files
-  sendMutation: request => wsrr.request('query', request.getQueryString()),
-  sendQueries: requests =>
-    wsrr.request('query', requests.map(request => request.getQueryString())),
+  sendMutation: async function sendMutation(request) {
+    const result = await wsrr.request('query', {
+      query: request.getQueryString(),
+      variables: request.getVariables(),
+    })
+    if (result.errors) return request.reject(result.errors.join(['\n']))
+    request.resolve({ response: result.data })
+  },
+  sendQueries: async function sendQueries(requests) {
+    const results = await wsrr.request('query', {
+      queries: requests.map(request => request.getQueryString()),
+    })
+
+    results.forEach((result, i) => {
+      if (result.hasOwnProperty('errors')) {
+        return requests[i].reject(result.errors.join('\n'))
+      }
+
+      requests[i].resolve({ response: result.data })
+    })
+  },
   supports: () => false,
 })
