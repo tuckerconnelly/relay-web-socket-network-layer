fd9cc81fd7e3173b56dacb98ca442356f2b957af
Tucker Connelly <web@tuckerconnelly.com>
2016-08-23 05:58:10 -0400

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 export default ops => ({
   // TODO Handle files
-  sendMutation: async function sendMutation(request) {
+  async sendMutation(request) {
     const result = await ops.request('query', {
       query: request.getQueryString(),
       variables: request.getVariables(),
     })
+
     if (result.errors) return request.reject(result.errors.join(['\n']))
     request.resolve({ response: result.data })
   },
-  sendQueries: async function sendQueries(requests) {
+  async sendQueries(requests) {
     const results = await ops.request('query', {
       queries: requests.map(request => request.getQueryString()),
     })
 
     results.forEach((result, i) => {
-      if (result.hasOwnProperty('errors')) {
-        return requests[i].reject(result.errors.join('\n'))
-      }
-
+      if (result.errors) return requests[i].reject(result.errors.join('\n'))
       requests[i].resolve({ response: result.data })
     })
   },
